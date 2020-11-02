ebad12bbbe5179750ec12b12efb02f0695185e86
sandrinejoy <sandrinejoy007@gmail.com>
2020-11-03 02:39:56 +0530

    day1b

--- public/scripts/proctoredindex.js
+++ public/scripts/proctoredindex.js
@@ -1,4 +1,10 @@
+var HOST = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(HOST);
+var el;
 
+ws.onmessage = function (event) {
+ console.log('Server time: ' + event.data);
+};
 
 //actual code
 
